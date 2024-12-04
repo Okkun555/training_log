9bbacbf13fdf3828a36ef69402a22ec0afb60f1e
Okkun555 <y.okuno.btm@gmail.com>
2024-12-04 21:49:53 +0900

    種目作成時のバリデーション追加

--- back/app/models/training_item.rb
+++ back/app/models/training_item.rb
@@ -4,5 +4,8 @@ class TrainingItem < ApplicationRecord
   enum :weight_unit, {
     kg: 1,
     lib: 2
-  }
+  }, validate: true
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :weight_unit, presence: true
 end
